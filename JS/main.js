504865ec31ca84bd872fe641a463c1b84b696e85
Daniel Sebastian Álvarez Rodríguez <daniel.alvarezro96@gmail.com>
2025-04-10 09:45:21 -0600

    Se añaden los elementos faltantes

--- JS/main.js
+++ JS/main.js
@@ -22,6 +22,17 @@ console.log("otroElemento: ", otroElemento);
 let otrosElementos = document.querySelectorAll("ul>li");
 console.log(otrosElementos.length);
 
+let txtRFC = document.getElementById("txtRFC");
+let txtTelefono = document.getElementById("txtTelefono");
+let txtCURP = document.getElementById("txtCURP");
+
+let contador = 0;
+function modifica() {
+  //encabezado1.innerText = "Ejercicio DOM";
+  encabezado1.innerHTML = "<em>Ejercicio</em> DOM";
+  encabezado2.innerText = ++contador;
+} // modifica
+
 btnMostrar.addEventListener("click", function (event) {
   event.preventDefault(); // No hagas lo que haces por defecto.
 
@@ -34,14 +45,74 @@ btnMostrar.addEventListener("click", function (event) {
   // SOLO PUEDE EXISTIR EN UN LUGAR A LA VEZ, SIEMPRE TOMA COMO REFERENCIA LA ÚLTIMA LÍNEA LEÍDA
 
   // before: Inserta el elemento antes de la lista
-  listas.item(0).before(element);
+  // listas.item(0).before(element);
 
   // prepend: Inserta el elemento al principio de la lista
-  listas.item(0).prepend(element2);
+  // listas.item(0).prepend(element2);
 
   // append: Inserta el elemento al final de la lista
-  listas.item(0).append(element);
+  // listas.item(0).append(element);
 
   // after: Inserta el elemento después de la lista
-  listas.item(0).after(element2);
+  // listas.item(0).after(element2);
+
+  // listas.item(1).insertAdjacentElement("afterbegin", element);
+  // listas.item(1).insertAdjacentElement("beforeend", element2);
+
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "beforebegin",
+      `<li class="list-group-item">Before begin item</li>`
+    );
+
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "afterend",
+      `<li class="list-group-item">After end item</li>`
+    );
+
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "afterbegin",
+      `<li class="list-group-item">After begin item</li>`
+    );
+
+  listas
+    .item(1)
+    .insertAdjacentHTML(
+      "beforeend",
+      `<li class="list-group-item">Before end item</li>`
+    );
+}); //btnMostrar
+
+// Se ejecuta cuando termina de cargar todos los elementos de la página
+window.addEventListener("load", function (event) {
+  console.log("Se terminó de cargar la página");
+}); // Load
+
+function txtToUpper(event) {
+  event.target.value = event.target.value.trim().toUpperCase();
+}
+
+//blur -> cuando se sale del campo
+// txtRFC.addEventListener("blur", function(event){
+//     event.preventDefault();
+//     txtRFC.value = txtRFC.value.toUpperCase();
+// });//txtRFC
+
+// txtCURP.addEventListener("blur", function(event){
+//     event.preventDefault();
+//     txtCURP.value = txtCURP.value.toUpperCase();
+// });//txtRFC
+
+// Blur -> cuando sale del campo
+txtRFC.addEventListener("blur", txtToUpper);
+txtCURP.addEventListener("blur", txtToUpper);
+
+txtTelefono.addEventListener("blur", function (event) {
+  event.preventDefault();
+  txtTelefono.value = txtTelefono.trim().value.slice(0, 10);
 });
